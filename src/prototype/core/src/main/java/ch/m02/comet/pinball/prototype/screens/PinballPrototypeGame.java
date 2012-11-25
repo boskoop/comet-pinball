3a88ef52ff98564d08a8f0452f781af9ae7f6ece
Patrick Haring <hip4@bfh.ch>
2012-11-25 22:54:44 +0100

    Added collision detection and sounds

--- src/prototype/core/src/main/java/ch/m02/comet/pinball/prototype/screens/PinballPrototypeGame.java
+++ src/prototype/core/src/main/java/ch/m02/comet/pinball/prototype/screens/PinballPrototypeGame.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -13,7 +14,12 @@ import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.Manifold;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 
@@ -29,9 +35,15 @@ public class PinballPrototypeGame implements Screen {
 
 	private static final int WINDOW_WIDTH = 512, WINDOW_HEIGHT = 1024;
 	private World world;
+	
+	private Sound pinballMachineSound,hornSound;
 
 	// private static final float WORLD_TO_BOX = 0.01f, BOX_WORLD_TO = 100f;
 	private Box2DDebugRenderer debugRender;
+	
+	private Fixture groundFixture, ceilingFixture, ballFixture;
+	
+	
 
 //	private Game game;
 
@@ -78,7 +90,7 @@ public class PinballPrototypeGame implements Screen {
 		fixtureDef.restitution = 0.6f; // Make it bounce a little bit
 
 		/* Fixture circleFixture = */
-		circleBody.createFixture(fixtureDef);
+		ballFixture = circleBody.createFixture(fixtureDef);
 
 		/*
 		 * Static Bodies
@@ -96,7 +108,7 @@ public class PinballPrototypeGame implements Screen {
 		PolygonShape groundBox = new PolygonShape();
 		groundBox.setAsBox(camera.viewportWidth * 2, borderThickness);
 
-		groundBody.createFixture(groundBox, 0.0f);
+		groundFixture = groundBody.createFixture(groundBox, 0.0f);
 
 		// Create body of ceiling
 		BodyDef ceilingBodyDef = new BodyDef();
@@ -107,12 +119,65 @@ public class PinballPrototypeGame implements Screen {
 		PolygonShape ceilingBox = new PolygonShape();
 		ceilingBox.setAsBox(camera.viewportWidth * 2, borderThickness);
 
-		ceilingBody.createFixture(ceilingBox, 0.0f);
+		ceilingFixture = ceilingBody.createFixture(ceilingBox, 0.0f);
 
 		// This debugger is useful for testing purposes
 		debugRender = new Box2DDebugRenderer();
+		
+		// Load sounds
+		
+		pinballMachineSound = loadSound("pinball-machine.mp3");
+		hornSound = loadSound("horn.mp3");
+		
+		// Create contact listener
+		ContactListener groundContactListener = new ContactListener() {
+			// http://libgdx.badlogicgames.com/nightlies/docs/api/com/badlogic/gdx/physics/box2d/ContactListener.html
+			
+			@Override
+			public void postSolve(Contact arg0, ContactImpulse arg1) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void endContact(Contact arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void beginContact(Contact contact) {
+				//pinballMachineSound.play();
+				
+				if(contact.getFixtureA() == ballFixture && contact.getFixtureB() == groundFixture 
+						|| contact.getFixtureB() == ballFixture && contact.getFixtureA() == groundFixture)
+					hornSound.play();
+				
+				if(contact.getFixtureA() == ballFixture && contact.getFixtureB() == ceilingFixture
+						|| contact.getFixtureB() == ballFixture && contact.getFixtureA() == ceilingFixture)
+					pinballMachineSound.play();
+				
+			}
 
+			@Override
+			public void preSolve(Contact arg0, Manifold arg1) {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+
+		world.setContactListener(groundContactListener);
 	}
+	
+	
+	/**
+	 * Load sound file
+	 * @param filename name of file ( relative to data/sounds/ )
+	 * @return Sound file
+	 */
+	private static Sound loadSound (String filename) {  
+        return Gdx.audio.newSound(Gdx.files.internal("data/sounds/" + filename));  
+    } 
 
 	@Override
 	public void dispose() {
@@ -147,6 +212,11 @@ public class PinballPrototypeGame implements Screen {
 		if (Gdx.input.isKeyPressed(Keys.RIGHT))
 			circleBody.applyForceToCenter(2000, 0);
 
+		if (Gdx.input.isKeyPressed(Keys.Q)){
+			pinballMachineSound.play();
+		}
+		
+		
 		// update actors and sprites
 		/*
 		 * Iterator<Body> bodies = world.getBodies();
@@ -161,7 +231,7 @@ public class PinballPrototypeGame implements Screen {
 
 		// Render physics should be called before physical rendering
 		debugRender.render(world, camera.combined);
-
+		
 		// step/update the world
 		// param1 = timestep 1/60 of a second...
 		// param2 = velocityIterations
