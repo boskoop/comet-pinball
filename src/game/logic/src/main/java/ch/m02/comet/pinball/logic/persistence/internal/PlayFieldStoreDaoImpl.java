a0e7fa8294d9864e8afd9af495f02d10469a4108
Christian Buergi <burgc5@bfh.ch>
2013-01-13 16:36:54 +0100

    added creation of default config file if no config exists tuned exception/stream handling in io classes

--- src/game/logic/src/main/java/ch/m02/comet/pinball/logic/persistence/internal/PlayFieldStoreDaoImpl.java
+++ src/game/logic/src/main/java/ch/m02/comet/pinball/logic/persistence/internal/PlayFieldStoreDaoImpl.java
@@ -3,6 +3,7 @@ package ch.m02.comet.pinball.logic.persistence.internal;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
@@ -12,6 +13,7 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,12 +26,28 @@ public class PlayFieldStoreDaoImpl implements PlayFieldStoreDao {
 	private static final Logger log = LoggerFactory.getLogger(PlayFieldStoreDaoImpl.class);
 	
 	static final String PERSISTENT_FILE_NAME = "playfields.xml";
+	static final String DEFAULT_CONFIG_FILE_NAME = "default-playfield.xml";
 
 	@PostConstruct
 	public void initializePersistence() {
 		File persistentFile = new File(PERSISTENT_FILE_NAME);
 		if (!persistentFile.exists()) {
-			throw new RuntimeException("playfield configuration file does not exist!");
+			log.info("No playfield config file found, creating default '{}'", PERSISTENT_FILE_NAME);
+			InputStream input = null;
+			FileWriter output = null;
+			try {
+				input = PlayFieldStoreDao.class.getResourceAsStream(DEFAULT_CONFIG_FILE_NAME);
+				persistentFile.createNewFile();
+				output = new FileWriter(persistentFile);
+				IOUtils.copy(input, output);
+			} catch (IOException e) {
+				log.error("Error while saving default config file", e);
+				throw new RuntimeException("Error while saving default config file", e);
+			} finally {
+				IOUtils.closeQuietly(input);
+				IOUtils.closeQuietly(output);
+			}
+			
 		}
 	}
 	
@@ -45,20 +63,16 @@ public class PlayFieldStoreDaoImpl implements PlayFieldStoreDao {
 			store = (PlayFieldStore) u.unmarshal(playfieldStream);
 		} catch (JAXBException e) {
 			log.error("Could not load playfield", e);
-			throw new RuntimeException("invalid playfield configuration file!");
+			throw new RuntimeException("invalid playfield configuration file!", e);
 		} catch (FileNotFoundException e) {
 			log.error("Could not load playfield", e);
-			throw new RuntimeException("playfield configuration file does not exist!");
+			throw new RuntimeException("playfield configuration file does not exist!", e);
 		} finally {
-			try {
-				playfieldStream.close();
-			} catch (IOException e) {
-				log.error("Exception while closing playfieldStream", e);
-			}
+			IOUtils.closeQuietly(playfieldStream);
 		}
 		log.info("loaded {} play fields", store.getPlayFields().size());
 		for (PlayFieldPdo field : store.getPlayFields()) {
-			log.info("loaded play field {}", field.getName());
+			log.info("loaded play field '{}'", field.getName());
 		}
 		return store.getPlayFields();
 	}
