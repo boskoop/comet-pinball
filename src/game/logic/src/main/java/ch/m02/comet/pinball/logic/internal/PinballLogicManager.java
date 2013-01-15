9739ea2d23d9bcd455c71a84a81c723b4cc678a4
Christian Buergi <burgc5@bfh.ch>
2013-01-15 21:08:18 +0100

    updated presentation

--- src/game/logic/src/main/java/ch/m02/comet/pinball/logic/internal/PinballLogicManager.java
+++ src/game/logic/src/main/java/ch/m02/comet/pinball/logic/internal/PinballLogicManager.java
@@ -2,15 +2,11 @@ package ch.m02.comet.pinball.logic.internal;
 
 import javax.inject.Inject;
 
-import ch.m02.comet.pinball.core.presentation.PresentationManager;
 import ch.m02.comet.pinball.logic.LogicManager;
 import ch.m02.comet.pinball.logic.State;
 import ch.m02.comet.pinball.logic.internal.state.StateContext;
 
 public class PinballLogicManager implements LogicManager {
-	
-	@Inject
-	private PresentationManager presentationManager;
 	
 	@Inject
 	private StateContext stateContext;
